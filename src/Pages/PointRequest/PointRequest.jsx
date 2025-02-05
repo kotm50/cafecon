707927aa44fed82d294a31ece4da1b23bd4b5e4f
kotm50 <koreatm50@gmail.com>
2025-02-05 11:37:36 +0900

    충전안내 문구수정

--- src/Pages/PointRequest/PointRequest.jsx
+++ src/Pages/PointRequest/PointRequest.jsx
@@ -6,7 +6,9 @@ function PointRequest() {
         <button
           className="block w-[200px] h-[200px] mx-auto text-xl font-extra bg-green-600 hover:bg-opacity-80 text-white rounded-lg"
           onClick={() =>
-            alert("포인트 충전은\n고객센터 : 1644-4223 으로 문의해 주세요")
+            alert(
+              "코티 고객사를 위한 서비스 입니다.\n코티 고객사시라면 고객센터로 문의주세요.\n이용해 주셔서 감사합니다.\n고객센터 : 1644-4223"
+            )
           }
         >
           포인트 신청하기
